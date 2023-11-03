f5b93a1523781939c8fadd1ec53e39fb3f480c37
hongdiem_roro <mingukmanse673@gmail.com>
2023-11-03 08:28:57 +0700

    socket

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const { Server } = require("socket.io");
 
-const io = new Server({ cors: "http://localhost:8081" });
+const io = new Server({ cors: "http://localhost:8080" });
+
+let onlineUsers = []
 
 io.on("connection", (socket) => {
   console.log("new connection: ", socket.id);
 
+ 
+  socket.on("addNewUser", (userID) => {
+    !onlineUsers.some((user) => user.userID === userID) &&
+      onlineUsers.push({
+        userID,
+        socketID: socket.id,
+      })
+      console.log("onlineUsers", onlineUsers);
+
+      io.emit("getOnlineUsers", onlineUsers);
+  })
+
+
   socket.on("send-message", (data) => {
-    socket.broadcast.emit("recieve-message", data);
+    console.log(data._idSession);
+    console.log(onlineUsers);
+    const user = onlineUsers.find(user => user.userID == data._idSession)
+    console.log(user);
+    if(user){
+      socket.to(user.socketID).emit("recieve-message", data);
+    }
+  })
+
+  socket.on("disconnect", () => {
+    onlineUsers = onlineUsers.filter(user => user.socketID !== socket.id);
+    io.emit("getOnlineUsers", onlineUsers);
   })
 });
 
